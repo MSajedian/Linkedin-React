fc8477e26f8f9b94b81000eea6cbe8030cbc3f2e
Ekow <ewusiessel@gmail.com>
2021-05-12 16:31:45 +0100

    Added footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Dashboard from "./components/DashBoard";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Dashboard />
-     
+      <Footer />
     </>
   );
 }
