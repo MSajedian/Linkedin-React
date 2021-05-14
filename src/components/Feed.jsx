728fd3e717738d5539f029fedb204bdbbd6d1f8c
MSajedian <mohammadsajedian@gmail.com>
2021-05-14 16:28:58 +0100

    post photo updated

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -9,11 +9,16 @@ import {
 	Row,
 } from "react-bootstrap"
 
+
+// let array = [1, 2, 3]
+// console.log('array.filter', array.filter(element => { return element === 3 }))
+
 export default class Feed extends Component {
 	state = {
 		postInput: "",
 		posts: [],
-		posted: 0
+		posted: 0,
+		postFile: null
 	}
 
 	componentDidMount = async () => {
@@ -27,17 +32,19 @@ export default class Feed extends Component {
 					headers,
 				}
 			)
-			const posts = await response.json()
-			this.setState({ posts: posts })
-			// console.log(posts.slice(-20))
+			if (response.ok) {
+				const posts = await response.json()
+				this.setState({ posts: posts })
+			}
 		} catch (error) {
 			console.log("You have an error fetching:", error)
 		}
+
 	}
 
 	handleSubmit = async () => {
 		const postInput = { text: this.state.postInput }
-		const headers = {
+		let headers = {
 			Authorization: "Bearer " + this.props.token,
 			"Content-Type": "application/json",
 		}
@@ -56,6 +63,29 @@ export default class Feed extends Component {
 			console.log("You have an error posting:", error)
 		}
 		this.componentDidMount()
+		if (this.state.postFile) {
+			var myHeaders = new Headers();
+			myHeaders.append("Authorization", "Bearer " + this.props.token);
+
+			const myPosts = this.state.posts.slice(-100).filter(element => element.user._id === this.props.user._id)
+			const myLastPostId = myPosts.slice(-1)[0]._id
+			console.log('myLastPost:', myLastPostId)
+
+			var formdata = new FormData();
+			formdata.append("post", this.state.postFile);
+			console.log('formdata.getAll:', formdata.getAll('post'))
+			var requestOptions = {
+				method: 'POST',
+				headers: myHeaders,
+				body: formdata,
+				redirect: 'follow'
+			};
+			
+			fetch(`https://striveschool-api.herokuapp.com/api/posts/${myLastPostId}`, requestOptions)
+				.then(response => response.text())
+				.then(result => console.log(result))
+				.catch(error => console.log('error', error));
+		}
 	}
 	handleDelete = async (e) => {
 		console.log(e.target.value)
@@ -167,6 +197,7 @@ export default class Feed extends Component {
 										width: "80%",
 										display: "flex",
 										justifyContent: "space-evenly",
+										alignItems: 'center'
 									}}
 								>
 									<Button
@@ -188,11 +219,9 @@ export default class Feed extends Component {
 											<polygon points="14 2 18 6 7 17 3 17 3 13 14 2"></polygon>
 											<line x1="3" y1="22" x2="21" y2="22"></line>
 										</svg> Post
-													</Button>
-									{/* <Button className="mx-2 bg-white" variant="light">
-										Photo
-									</Button> */}
-									<label for="input-picture" className="btn btn-primary mx-2 bg-white text-dark border-0">
+									</Button>
+
+									<label for="input-picture" className="btn btn-outline mx-2 bg-white text-dark border-0">
 										<svg
 											xmlns="http://www.w3.org/2000/svg"
 											width="24"
@@ -211,7 +240,12 @@ export default class Feed extends Component {
 									</label>
 									<input onChange={e => {
 										this.setState({ postFile: e.target.files[0] })
-									}} style={{ display: 'none' }} type="file" id="input-picture" />
+										console.log('e.target.files:', e.target.files[0])
+									}}
+										type="file"
+										id="input-picture"
+										style={{ display: 'none' }}
+									/>
 
 									<Button className="mx-2 bg-white" variant="light">
 										<svg
@@ -247,7 +281,7 @@ export default class Feed extends Component {
 						</Card>
 						<br />
 						{this.state.posts
-							.slice(-4)
+							.slice(-10)
 							.reverse()
 							.map((post) => (
 								<Card key={post._id} className="my-2">
@@ -259,9 +293,8 @@ export default class Feed extends Component {
 											<img
 												style={{ width: "48px", height: "48px" }}
 												src={post.user.image}
-												alt=""
-											></img>
-
+												alt="postuserimage"
+											/>
 											<div className="ms-2 d-flex flex-column">
 												<b className="ml-2">{post.user.name}</b>
 												<span className="text-muted ml-2">
@@ -286,11 +319,27 @@ export default class Feed extends Component {
 
 										<div
 											style={{
-												display: "inline-block",
-												padding: "12px 12px 0px 12px",
+												// display: "inline-block",
+												// padding: "12px 12px 0px 12px",
 											}}
 										>
-											{post.text}
+											<div
+												style={{
+													display: "inline-block",
+													padding: "12px 12px 12px 12px",
+												}}
+											>
+												{post.text}<br />
+												{post._id}
+											</div>
+											{/* <div className="text-center" > */}
+											<img
+												className="img-fluid rounded mx-auto px-5 d-block"
+												// style={{ width: "48px", height: "48px" }}
+												src={post.image}
+												alt=""
+											/>
+											{/* </div> */}
 										</div>
 
 										<div style={{ objectFit: "contain" }}></div>
@@ -390,150 +439,10 @@ export default class Feed extends Component {
 									</Col>
 								</Card>
 							))}
-						<Card>
-							<Col xs={12} className="p-0 mb-4 section-outer">
-								<div
-									style={{ padding: "12px 12px 0px 12px" }}
-									className="d-flex"
-								>
-									<img
-										style={{ width: "48px", height: "48px" }}
-										src="https://media-exp1.licdn.com/dms/image/C4D0BAQF2N4oLrjT9uQ/company-logo_200_200/0/1607100764613?e=1628726400&v=beta&t=-ym0gdwneMH_gEiNV96nA4QASLign5VOBrEyAh9LBQ4"
-										alt=""
-									></img>
-
-									<div className="ms-2 d-flex flex-column">
-										<span>LinkedIn News UK</span>
-										<span className="text-muted">863,439 followers</span>
-										<span className="text-muted">
-											1w ·
-											<svg
-												xmlns="http://www.w3.org/2000/svg"
-												viewBox="0 0 16 16"
-												data-supported-dps="16x16"
-												fill="currentColor"
-												className="mercado-match"
-												width="16"
-												height="16"
-												focusable="false"
-											>
-												<path d="M8 1a7 7 0 107 7 7 7 0 00-7-7zM3 8a5 5 0 011-3l.55.55A1.5 1.5 0 015 6.62v1.07a.75.75 0 00.22.53l.56.56a.75.75 0 00.53.22H7v.69a.75.75 0 00.22.53l.56.56a.75.75 0 01.22.53V13a5 5 0 01-5-5zm6.24 4.83l2-2.46a.75.75 0 00.09-.8l-.58-1.16A.76.76 0 0010 8H7v-.19a.51.51 0 01.28-.45l.38-.19a.74.74 0 01.68 0L9 7.5l.38-.7a1 1 0 00.12-.48v-.85a.78.78 0 01.21-.53l1.07-1.09a5 5 0 01-1.54 9z"></path>
-											</svg>
-										</span>
-									</div>
-
-									<div className="ms-auto">
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											viewBox="0 0 24 24"
-											data-supported-dps="24x24"
-											fill="currentColor"
-											className="mercado-match"
-											width="24"
-											height="24"
-											focusable="false"
-										>
-											<path d="M14 12a2 2 0 11-2-2 2 2 0 012 2zM4 10a2 2 0 102 2 2 2 0 00-2-2zm16 0a2 2 0 102 2 2 2 0 00-2-2z"></path>
-										</svg>
-									</div>
-								</div>
-
-								<div style={{ padding: "12px 12px 0px 12px" }}>
-									Ooh yeah linkedIn is great!
-								</div>
-
-								<div style={{ objectFit: "contain" }}></div>
-
-								<div style={{ padding: "12px 12px 0px 12px" }}>
-									<img
-										src="https://static-exp1.licdn.com/sc/h/d310t2g24pvdy4pt1jkedo4yb"
-										alt="LIKE"
-									></img>
-									<span className="text-muted"> 69 · 0 comments </span>
-								</div>
-
-								<div style={{ padding: "0px 12px 0px 12px" }}>
-									<hr></hr>
-								</div>
-								<div className="mb-3 d-flex px-3">
-									<div className="d-flex align-items-center">
-										<Button variant="light" className="mx-2 text-muted">
-											<svg
-												xmlns="http://www.w3.org/2000/svg"
-												viewBox="0 0 24 24"
-												data-supported-dps="24x24"
-												fill="currentColor"
-												className="mercado-match"
-												width="24"
-												height="24"
-												focusable="false"
-											>
-												<path d="M19.46 11l-3.91-3.91a7 7 0 01-1.69-2.74l-.49-1.47A2.76 2.76 0 0010.76 1 2.75 2.75 0 008 3.74v1.12a9.19 9.19 0 00.46 2.85L8.89 9H4.12A2.12 2.12 0 002 11.12a2.16 2.16 0 00.92 1.76A2.11 2.11 0 002 14.62a2.14 2.14 0 001.28 2 2 2 0 00-.28 1 2.12 2.12 0 002 2.12v.14A2.12 2.12 0 007.12 22h7.49a8.08 8.08 0 003.58-.84l.31-.16H21V11zM19 19h-1l-.73.37a6.14 6.14 0 01-2.69.63H7.72a1 1 0 01-1-.72l-.25-.87-.85-.41A1 1 0 015 17l.17-1-.76-.74A1 1 0 014.27 14l.66-1.09-.73-1.1a.49.49 0 01.08-.7.48.48 0 01.34-.11h7.05l-1.31-3.92A7 7 0 0110 4.86V3.75a.77.77 0 01.75-.75.75.75 0 01.71.51L12 5a9 9 0 002.13 3.5l4.5 4.5H19z"></path>
-											</svg>
-											Like
-										</Button>
-									</div>
-
-									<div className="d-flex align-items-center">
-										<Button variant="light" className="mx-2 text-muted">
-											<svg
-												xmlns="http://www.w3.org/2000/svg"
-												viewBox="0 0 24 24"
-												data-supported-dps="24x24"
-												fill="currentColor"
-												className="mercado-match"
-												width="24"
-												height="24"
-												focusable="false"
-											>
-												<path d="M7 9h10v1H7zm0 4h7v-1H7zm16-2a6.78 6.78 0 01-2.84 5.61L12 22v-4H8A7 7 0 018 4h8a7 7 0 017 7zm-2 0a5 5 0 00-5-5H8a5 5 0 000 10h6v2.28L19 15a4.79 4.79 0 002-4z"></path>
-											</svg>
-											Comment
-										</Button>
-									</div>
-
-									<div className="d-flex align-items-center">
-										<Button variant="light" className="mx-2 text-muted ">
-											<svg
-												xmlns="http://www.w3.org/2000/svg"
-												viewBox="0 0 24 24"
-												data-supported-dps="24x24"
-												fill="currentColor"
-												className="mercado-match"
-												width="24"
-												height="24"
-												focusable="false"
-											>
-												<path d="M23 12l-4.61 7H16l4-6H8a3.92 3.92 0 00-4 3.84V17a4 4 0 00.19 1.24L5.12 21H3l-.73-2.22A6.4 6.4 0 012 16.94 6 6 0 018 11h12l-4-6h2.39z"></path>
-											</svg>
-											Share
-										</Button>
-									</div>
-
-									<div className="d-flex align-items-center">
-										<Button variant="light" className="mx-2 text-muted">
-											<svg
-												xmlns="http://www.w3.org/2000/svg"
-												viewBox="0 0 24 24"
-												data-supported-dps="24x24"
-												fill="currentColor"
-												className="mercado-match"
-												width="24"
-												height="24"
-												focusable="false"
-											>
-												<path d="M21 3L0 10l7.66 4.26L16 8l-6.26 8.34L14 24l7-21z"></path>
-											</svg>
-											Send
-										</Button>
-									</div>
-								</div>
-							</Col>
-						</Card>
 					</Col>
 					<Col className="col-2"></Col>
 				</Row>
-			</Container>
+			</Container >
 		)
 	}
 }
